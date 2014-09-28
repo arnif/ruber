1a7b0572521d5ec3ddde0e46bc484b34bf5cd002
sindris12 <sindris12@ru.is>
2014-09-28 23:37:30 +0000

    fokk this

--- src/test/is/ru/honn/ruber/test/TestRuberService.java
+++ src/test/is/ru/honn/ruber/test/TestRuberService.java
@@ -1,11 +1,14 @@
 package is.ru.honn.ruber.test;
 
+import is.ru.honn.ruber.domain.User;
 import is.ru.honn.ruber.service.RuberService;
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -24,20 +27,29 @@ public class TestRuberService extends TestCase
   @Before
   public void setUp() throws Exception
   {
-
+      ApplicationContext context= new ClassPathXmlApplicationContext("app-test-stub.xml");
+      User user = (User)context.getBean("user");
   }
 
   @Test
   public void testUser()
   {
-    log.info("testUser");
-    fail();
+      log.info("testUser");
+
+      User user = new User("lol", "sindris12", "Sindri", "Sigurjonsson", "1234", "sindris12@ru.is", "pic", "promofreestuff");
+
+      service.signup(user);
+
+      assertEquals("Sindri", service.getUser("sindris12").getFirstName());
+
+
+      //fail();
   }
 
   @Test
   public void testActivity()
   {
     log.info("testActivity");
-    fail();
+    //fail();
   }
 }
